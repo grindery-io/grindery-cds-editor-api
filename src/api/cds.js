2619caa7aef5f11074b56313602520c84deb7fdd
Egor Miliukov <milukove@gmail.com>
2022-11-04 01:14:55 +0200

    temporary update cloning

--- src/api/cds.js
+++ src/api/cds.js
@@ -194,15 +194,15 @@ cds.post("/clone", auth.isRequired, async (req, res) => {
 
   const connector = JSON.parse(cds);
 
-  const key = `${connector.key}_clone_${Math.floor(Date.now() / 1000)}`;
-  const name = connector.name ? `${connector.name} clone` : "";
+  const key = `${connector.key}`;
+  const name = connector.name ? `${connector.name}` : "";
 
   const data = {
     cds: JSON.stringify({ ...connector, key, name }),
     name,
     icon: connector.icon || "",
     description: connector.description || "",
-    user: res.locals.userId || "",
+    user: "",
     workspace: res.locals.workspaceId || "",
   };
 
