2cd26e8bc9b3d99fd778f1ca9a4817f622292d1c
Egor Miliukov <milukove@gmail.com>
2022-11-16 00:58:56 +0200

    remove auto-publishing

--- src/publishProd.js
+++
@@ -1,5 +0,0 @@
-const routines = require("./utils/routines");
-
-routines.publishPendingConnectors().catch((err) => {
-  console.error("pending connectors publishing failed", err);
-});
--- src/publishStaging.js
+++
@@ -1,5 +0,0 @@
-const routines = require("./utils/routines");
-
-routines.publishPendingConnectors("staging").catch((err) => {
-  console.error("pending staging connectors publishing failed", err);
-});
